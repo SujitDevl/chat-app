65536d966d49928f520452909aa5cfe661e72d5a
Sujit <pandasujit687@gmail.com>
2025-02-25 00:16:28 +0530

    changes in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import "/src/style.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState([]);
+  const messageRef = useRef(null);
+
+  const sendMessage = () => {
+    const newMessage = messageRef.current.value.trim();
+    if (newMessage && username.trim()) {
+      setMessages([...messages, { user: username, text: newMessage }]);
+      messageRef.current.value = " ";
+    }
+  };
   return (
     <>
-    <h1>Its my first chat app using react.</h1>
+      <div className="chat-container">
+        <h2>Talky Penguin</h2>
+
+        <div className="username-container">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div className="chat-box">
+          {messages.map((msg, index) => (
+            <p key={index} className="message">
+              {msg}
+            </p>
+          ))}
+        </div>
+        <div className="input-container">
+          <input ref={messageRef} type="text" placeholder="Type a message" />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
